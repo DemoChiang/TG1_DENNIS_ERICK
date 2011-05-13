da897f4711d3a8b92d6b812dadee717b85d12e96
Dennis <chiangdennis@hotmail.com>
2011-05-13 07:12:58 -0600

    Agregue prestamo y cuota

---
+++ src/Cuota.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laptop
+ */
+class Cuota {
+    public double pagoMensual;
+    public boolean pagada;
+
+    public Cuota(double pm, boolean p){
+        pagoMensual = pm;
+        pagada = p;
+    }
+
+
+
+}
