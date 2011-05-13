0fe12bec05db68b81cfc018ed71319539f5a0606
Lerik <jj.cob@hotmail.com>
2011-05-13 16:33:19 +0200

    El Main esta listo

---
+++ src/PrestamoAuto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jacob Ramirez
+ */
+class PrestamoAuto extends Prestamo{
+
+    public PrestamoAuto(double Monto, String Nombre, int Cuotas, String Serie) {
+    }
+
+}
---
+++ src/PrestamoVivienda.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jacob Ramirez
+ */
+class PrestamoVivienda extends Prestamo{
+
+    public PrestamoVivienda(double Monto, String Nombre, int Cuotas, boolean Ban) {
+    }
+
+}
